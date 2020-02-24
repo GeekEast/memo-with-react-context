577d428db884947d77f33038a3d40ede6e34cb43
GeekEast <xiang.tan.p2109@gmail.com>
2020-02-24 22:40:26 +1030

    update smartStrictEqual

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,5 @@
 import deepEqual from 'fast-deep-equal';
-import shallowCompare from 'react-addons-shallow-compare';
+import { equal } from 'fast-shallow-equal';
 
 const getType = (sth) => {
   return Object.prototype.toString.call(sth).slice(8, -1);
@@ -15,22 +15,11 @@ const deepObject = (obj) => {
 }
 
 export const smartStrictEqual = (prev, next) => {
-  // console.log(prev);
-  // console.log(next);
   const prevType = getType(prev);
   const nextType = getType(next);
-  // console.log('type check ↓')
   if (prevType !== nextType) return Object.is(prev, next);
-  // console.log('array check ↓')
   if (prevType === 'Array') return deepEqual(prev, next);
-  // console.log('Object check ↓');
-  if (prevType !== 'Object') return Object.is(prev, next);
-  // console.log('Deep Object check ↓')
-  if (deepObject(prev) || deepObject(next)) {
-    // console.log(deepEqual(prev,next));
-    // console.log(_.isEqual(prev,next));
-    return deepEqual(prev, next)
-  }
-  // console.log('Shallow Object check')
-  return shallowCompare(prev, next)
+  if (prevType !== 'Object') return Object.is(prev, next)
+  if (deepObject(prev) || deepObject(next)) return deepEqual(prev, next)
+  return equal(prev, next)
 }
